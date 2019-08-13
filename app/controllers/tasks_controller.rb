66b5f435e2c100a9c46d4c6ac9bd4b25f1dd5056
Dominik Wojciechowski <dominik.woj@me.com>
2019-08-13 17:03:11 +0200

    complete excercise incl. delete feature

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -26,6 +26,12 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
 
   def tasks_params
